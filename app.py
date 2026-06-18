2cd54eba1028cf9774088bc08aaa260509c09c9b
Ayush Devadiga <ayushdevadiga2005@gmail.com>
2026-06-18 11:43:15 +0530

    The current state of app.py :  Base intro page was designed properly

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+# File paths 
+intro_path = "frontend\\introduction-page.md"
+
+def main():
+    st.set_page_config(
+                        page_title=" Customer Churn Prediction via XGBOOST",
+                        page_icon="🏢", 
+                        layout="wide", 
+                        initial_sidebar_state="expanded",
+                        menu_items={
+                                        'Get Help': None,
+                                        'Report a bug': None,
+                                        'About': None
+                                    }
+                    )
+    try :
+        with open(intro_path , 'r' , encoding = 'utf-8') as file:
+            intro_markdown = file.read() 
+
+        st.markdown(intro_markdown)
+
+    except Exception as e : 
+        print(f'The introduction-page was not found .\n Cause : {e}')
+
+if __name__ == '__main__':
+    main()
