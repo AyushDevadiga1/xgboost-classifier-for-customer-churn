01bda5e3f73a63eb5031722ef2a30af36b0e9634
Ayush Devadiga <ayushdevadiga2005@gmail.com>
2026-06-19 07:56:55 +0530

    Fixed rename issues and also a bit of text changes

--- pages/2_make_predictions.py
+++ pages/2_make_predictions.py
@@ -3,17 +3,19 @@ import pandas as pd
 from pathlib import Path
 from src.validator import validate_dataset
 
-# Maintain persistence across workspace view swaps
+# Flags to change the page interface depending on differnent session states 
+
 if "dataset_approved" not in st.session_state:
     st.session_state.dataset_approved = False
     st.session_state.processed_data = None
     st.session_state.data_status = None
 
-# File paths 
+# File paths for different assests
 PATH_INTRO = "frontend\\page-2\\introduction.md"
 PATH_SAMPLE = "frontend\\page-2\\data-sample.csv"
-def main():
 
+def main():
+    
     st.set_page_config(
                         page_title=" Customer Churn Prediction via XGBOOST",
                         page_icon="🔮", 
@@ -26,6 +28,9 @@ def main():
                                     }
                     )
     
+    # First condition to check if the dataset is not approved
+    # If the dataset is not approved we want the user to be still on home page 
+ 
     if not st.session_state.dataset_approved:
 
         try :
@@ -47,13 +52,13 @@ def main():
 
 
 
-        # 3. Create two side-by-side columns for Download and Upload
+        # Two side-by-side columns for Download and Upload
         col1, col2 = st.columns(2)
 
         with col1:
             st.subheader(" Get Template")
             st.download_button(
-                label="📥 Download Sample CSV",
+                label="⬇ Download Sample CSV",
                 data=sample_data,
                 file_name="telco_dataset_sample.csv",
                 mime="text/csv",
@@ -63,12 +68,13 @@ def main():
         with col2:
             st.subheader(" Submit Data ")
             uploaded_file = st.file_uploader(
-                label="Upload your completed Telco dataset",
+                label="⬆ Upload Your Sample CSV",
                 type=["csv"],
                 accept_multiple_files=False,
                 label_visibility="collapsed"
             )
 
+        # Check whether the file was actually uploaded or not
 
         if uploaded_file is not None:
 
@@ -81,6 +87,9 @@ def main():
                     user_df['TotalCharges'] = pd.to_numeric(user_df['TotalCharges'], errors='coerce')
                 
                 # Execute automated validation & structure detection
+                # Validatior is responsible for passing the status and if all flags are set then only
+                # can user continue or else the rejectede message will be displayed 
+
                 is_valid, status, message = validate_dataset(user_df)
                 
                 if is_valid:
@@ -97,36 +106,53 @@ def main():
             except Exception as e:
                 st.error(f"❌ Critical file reading error: {e}")
 
+    # Else block for the first conditional
+    # This means will run if condition becomes false - dataset_aprroved is Not None
+
     else:
-        st.title("📊 Telco Analytics Workspace")
+        # The default title
+        st.title("Telco Analytics Workspace")
         
-        if st.sidebar.button("🔄 Upload Different Dataset"):
+        # A sidebar to upload your own data
+        if st.sidebar.button(" Upload Different Dataset"):
             st.session_state.dataset_approved = False
             st.session_state.processed_data = None
             st.session_state.data_status = None
             st.rerun()
 
+        # Fetch the data and statuses
         df = st.session_state.processed_data
         status = st.session_state.data_status
         
-        st.sidebar.success("📋 Active Session: Validated Schema")
+        st.sidebar.success(" Active Session: Validated Schema")
         st.sidebar.metric(label="Total Records Loaded", value=len(df))
         
+        # Runs when the dataset has both X and y Columns and their flag was set 
         if status == "CN_AND_Y" or status == "X_AND_Y":
             st.sidebar.info("Detected Mode: **Training/Evaluation**")
             st.header("⚡ Evaluation Dashboard ($X$ and $y$)")
             
             # Explicit backend variables for model computations
-            X = df.drop(columns=["churn"])
-            y = df["churn"]
+            X = df.drop(columns=["Churn"])
+            y = df["Churn"]
             
-            st.dataframe(df)
+            # Split the screen into two columns: 4 parts for X, 1 part for y
+            col_x, col_y = st.columns([4, 1])
             
+            with col_x:
+                st.markdown("### Features ($X$)")
+                st.dataframe(X, use_container_width=True)
+                
+            with col_y:
+                st.markdown("### Label ($y$)")
+                st.dataframe(y, use_container_width=True)
+
+        # Runs only if X column conditions are met    
         elif status == "X_ONLY":
             st.sidebar.info("Detected Mode: **Inference Pipeline**")
             st.header("🔮 Inference Pipeline ($X$ only)")
             
-            X = df
+            X = df  
             
             st.dataframe(X)
 if __name__ == '__main__':
