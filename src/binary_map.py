e9651ae654ac7cc0841e191832c11235179524fc
Ayush Devadiga <ayushdevadiga2005@gmail.com>
2026-06-25 17:48:59 +0530

    Documentation

--- src/binary_map.py
+++ src/binary_map.py
@@ -1,4 +1,15 @@
 def to_binary_map(X_sliced):
+    """
+    Binary Map that value into : 
+            {
+                'Yes': 1,
+                'No': 0,
+                'No internet service': 0,
+                'No phone service': 0,
+                'Male': 1,
+                'Female': 0
+            }
+    """
     col_map = {
         'Yes': 1,
         'No': 0,
@@ -7,4 +18,4 @@ def to_binary_map(X_sliced):
         'Male': 1,
         'Female': 0
     }
-    return X_sliced.apply(lambda x: x.map(col_map).fillna(0))
+    return X_sliced.apply(lambda x: x.map(col_map).fillna(0)) # Fill any null values with default o
